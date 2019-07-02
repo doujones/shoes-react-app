54401e37497cc1c3c85622fdb102d4da71720c4f
Dou Jones <misterchiefexecutiveofficer@yahoo.com>
2019-07-01 20:04:06 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return ( <h1>
+    Hello
+  </h1>
+  ) 
+}
+
+export default App;
