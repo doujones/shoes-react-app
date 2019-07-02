425d69b9fdea1e319d735093ee9c7b6f21e7a067
Dou Jones <misterchiefexecutiveofficer@yahoo.com>
2019-07-02 08:19:44 -0400

    First Change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { FaSocks } from "react-icons/fa";
+import Home from './pages/HomePage'
+import About from './pages/AboutPage'
+import Products from './pages/ProductsPage'
+import Contact from './pages/ContactPage'
+import SingleProduct from './pages/SingleProductPage'
+import Default from './pages/DefaultPage.js'
+import Cart from './pages/CartPage.js'
 
-function App() {
-  return ( <h1>
-    Hello
-  </h1>
-  ) 
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+
+class App extends Component {
+  render(){
+    return (
+      <>
+
+
+    <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component= {Home}/>
+      <Route path="/about" component= {About}/>
+      <Route path="/contact" component= {Contact}/>
+      <Route path="/products" exact component= {Products}/>
+      <Route path="/products/:id" component= {SingleProduct}/>
+      <Route Path="/cart" component= {Cart}/>
+      <Route component= {Default}/>
+    </Switch>
+    </BrowserRouter>
+    </>
+    );
+  }
 }
+
+
 
 export default App;
---
+++ src/pages/ContactPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ContactPage() {
+  return (
+    <>
+      <h1>Hello from About Page</h1>
+    </>
+  )
+}
